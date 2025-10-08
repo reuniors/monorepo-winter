ba4742275907daa1f4c4f7fd8dc5d4f5a94a2b0e
Janko <jangrbovic@gmail.com>
2025-10-08 18:35:46 +0200

    Add initial implementation of the Botovi plugin
    - Created the main Plugin class with methods for component and settings registration.
    - Added plugin metadata in plugin.yaml, including name, description, author, and homepage.
    - Introduced language support with English translations in lang.php.
    - Initialized versioning with version.yaml to track plugin updates.

---
+++ plugins/reuniors/botovi/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Reuniors\botovi;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
