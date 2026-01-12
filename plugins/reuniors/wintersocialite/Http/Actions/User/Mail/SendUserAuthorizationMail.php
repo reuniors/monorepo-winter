edadc4e8d2f7ca03123c3f440a15d81ddca09c69
Janko <jangrbovic@gmail.com>
2026-01-12 22:45:52 +0100

    Enhance email sending functionality in SendUserAuthorizationMail
    - Updated the handle method to include a dynamic sender name for authorization emails, improving personalization and clarity in communication.
    - Utilized environment variables for sender address and name, ensuring flexibility and adherence to configuration standards.

--- plugins/reuniors/wintersocialite/Http/Actions/User/Mail/SendUserAuthorizationMail.php
+++ plugins/reuniors/wintersocialite/Http/Actions/User/Mail/SendUserAuthorizationMail.php
@@ -7,9 +7,13 @@ class SendUserAuthorizationMail extends BaseAction
 {
     public function handle(array $attributes = [], $user = null)
     {
+        $fromName = env('MAIL_FROM_NAME', env('APP_NAME', 'Winter CMS'));
+        
         Mail::sendTo($user, 'reuniors.wintersocialite::mail.authorization-confirmation', [
             'code' => $user->activation_code
-        ]);
+        ], function ($message) use ($fromName) {
+            $message->from(env('MAIL_FROM_ADDRESS', 'noreply@example.com'), $fromName);
+        });
     }
 
     public function asController($user = null): array
