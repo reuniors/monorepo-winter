0a26ababf8c046cdc6cbd18523ac840bb9763455
Janko <jangrbovic@gmail.com>
2026-01-12 22:50:57 +0100

    Update email subject in SendUserAuthorizationMail for improved clarity
    - Modified the handle method to set a localized subject for the authorization email, enhancing user experience and communication effectiveness.
    - Adjusted the sender name fallback to use 'CMS' for consistency with application branding.

--- plugins/reuniors/wintersocialite/Http/Actions/User/Mail/SendUserAuthorizationMail.php
+++ plugins/reuniors/wintersocialite/Http/Actions/User/Mail/SendUserAuthorizationMail.php
@@ -7,11 +7,12 @@ class SendUserAuthorizationMail extends BaseAction
 {
     public function handle(array $attributes = [], $user = null)
     {
-        $fromName = env('MAIL_FROM_NAME', env('APP_NAME', 'Winter CMS'));
+        $fromName = env('MAIL_FROM_NAME', env('APP_NAME', 'CMS'));
         
         Mail::sendTo($user, 'reuniors.wintersocialite::mail.authorization-confirmation', [
             'code' => $user->activation_code
         ], function ($message) use ($fromName) {
+            $message->subject(__('Potvrda email adrese'));
             $message->from(env('MAIL_FROM_ADDRESS', 'noreply@example.com'), $fromName);
         });
     }
